48ef3f19b88af938dc18051ed29f9782058198b5
ghostbuster91 <kghost0@gmail.com>
2018-11-19 21:43:49 +0100

    install jackson

--- src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
+++ src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
@@ -1,30 +1,41 @@
 package io.ghostbuster91.ktm.notifier.jitpack
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.post
 import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.netty.EngineMain
 import java.net.URL
 
-fun main(args: Array<String>) {
-    val port = Integer.valueOf(System.getenv("PORT"))
-    val server = embeddedServer(Netty, port = port) {
-        routing {
-            post("/demo") {
-                val event = call.receive<PushEvent>()
-                println(event)
-                val buildLog = URL("https://jitpack.io/com/github/${event.repository.fullName}/${event.after}").readText()
-                println(buildLog)
-                call.respond(HttpStatusCode.OK)
-            }
+@Suppress("UNUSED")
+fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
-    server.start(wait = true)
+    routing {
+        post("/demo") {
+            val event = call.receive<Map<String,Any>>()
+            println(event)
+            val buildLog = URL("https://jitpack.io/com/github/${(event["repository"] as Map<String,Any>)["fullName"]}/${event["after"]}").readText()
+            println("start")
+            println(buildLog)
+            println("end")
+            call.respond(HttpStatusCode.OK)
+        }
+    }
 }
 
-data class PushEvent(val repository: Repository,val after:String)
+
+fun main(args: Array<String>) = EngineMain.main(args)
+
+data class PushEvent(val repository: Repository, val after: String)
 data class Repository(val fullName: String)
