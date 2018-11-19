b73ba6715dbea608157779d4aae9d16babca7eee
ghostbuster91 <kghost0@gmail.com>
2018-11-19 20:10:36 +0100

    Init repo

---
+++ src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
@@ -0,0 +1,29 @@
+package io.ghostbuster91.ktm.notifier.jitpack
+
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.net.URL
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            post("/demo") {
+                val event = call.receive<PushEvent>()
+                println(event)
+                val buildLog = URL("https://jitpack.io/com/github/${event.repository.fullName}/${event.after}").readText()
+                println(buildLog)
+                call.respond(HttpStatusCode.OK)
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
+data class PushEvent(val repository: Repository,val after:String)
+data class Repository(val fullName: String)
