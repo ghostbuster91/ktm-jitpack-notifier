2902da90352f4c13643a5b41b706d75aba353db6
ghostbuster91 <kghost0@gmail.com>
2018-11-22 21:05:12 +0100

    Configure httpClient to use UserAgent header

--- src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
+++ src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
@@ -5,9 +5,11 @@ import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.client.HttpClient
-import io.ktor.client.features.json.GsonSerializer
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.request.get
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.HttpPlainText
+import io.ktor.client.request.header
+import io.ktor.client.request.request
+import io.ktor.client.request.url
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
@@ -16,10 +18,18 @@ import io.ktor.response.respond
 import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Suppress("UNUSED")
 fun Application.module() {
-    val client = HttpClient()
+    val client = HttpClient(OkHttp) {
+        install(HttpPlainText) {
+            defaultCharset = Charsets.UTF_8
+        }
+        engine {
+            addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.HEADERS })
+        }
+    }
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
@@ -27,14 +37,14 @@ fun Application.module() {
     }
     routing {
         post("/demo") {
-            val event = call.receive<Map<String,Any>>()
+            val event = call.receive<Map<String, Any>>()
             println(event)
-            val repository = event["repository"] as Map<String,Any>?
-            if(repository != null){
-                val buildLog = client.get<String>("https://jitpack.io/com/github/${repository["fullName"]}/${event["after"]}")
-                println("start")
-                println(buildLog)
-                println("end")
+            val repository = event["repository"] as Map<String, Any>?
+            if (repository != null) {
+                client.request<String> {
+                    header("user-agent", MOZILLA_USER_AGENT)
+                    url("https://jitpack.io/com/github/${repository["fullName"]}/${event["after"]}")
+                }
                 call.respond(HttpStatusCode.Created)
             }
             call.respond(HttpStatusCode.OK)
@@ -47,3 +57,5 @@ fun main(args: Array<String>) = EngineMain.main(args)
 
 data class PushEvent(val repository: Repository, val after: String)
 data class Repository(val fullName: String)
+
+val MOZILLA_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
