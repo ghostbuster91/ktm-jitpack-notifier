e7c1635b4745478bc62449d0d9e79c41c138cc0c
ghostbuster91 <kghost0@gmail.com>
2018-11-22 21:16:37 +0100

    Change json reference to camel-case

--- src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
+++ src/main/kotlin/io/ghostbuster91/ktm/notifier/jitpack/Main.kt
@@ -7,9 +7,9 @@ import io.ktor.application.install
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.features.HttpPlainText
+import io.ktor.client.features.defaultRequest
+import io.ktor.client.request.get
 import io.ktor.client.request.header
-import io.ktor.client.request.request
-import io.ktor.client.request.url
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
@@ -29,6 +29,9 @@ fun Application.module() {
         engine {
             addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.HEADERS })
         }
+        defaultRequest {
+            header("user-agent", MOZILLA_USER_AGENT)
+        }
     }
     install(ContentNegotiation) {
         jackson {
@@ -41,10 +44,7 @@ fun Application.module() {
             println(event)
             val repository = event["repository"] as Map<String, Any>?
             if (repository != null) {
-                client.request<String> {
-                    header("user-agent", MOZILLA_USER_AGENT)
-                    url("https://jitpack.io/com/github/${repository["fullName"]}/${event["after"]}")
-                }
+                client.get<String>("https://jitpack.io/com/github/${repository["full_name"]}/${event["after"]}")
                 call.respond(HttpStatusCode.Created)
             }
             call.respond(HttpStatusCode.OK)
@@ -54,8 +54,5 @@ fun Application.module() {
 
 
 fun main(args: Array<String>) = EngineMain.main(args)
-
-data class PushEvent(val repository: Repository, val after: String)
-data class Repository(val fullName: String)
 
 val MOZILLA_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
